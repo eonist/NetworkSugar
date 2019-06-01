6d391cbd4de4d5fbcd766bf4d1e39fc79d2abad2
Eonist <30n1st@gmail.com>
2019-06-01 08:43:06 +0200

    Add httpMethod to str method

--- NetworkSugar/src/NetworkParser+String.swift
+++ NetworkSugar/src/NetworkParser+String.swift
@@ -6,11 +6,13 @@ import Foundation
 extension NetworkParser {
    /**
     * Return string for WebPath
+    * - Remark: should be added to a bg thread
+    * - Remark: For multiple varaiables etc: param1=value1&param2=value2
     * ## Examples:
     * NetworkParser.str(webPath: webPath) //(See defaultDownloadComplete)
-    * Fixme: ⚠️️ rename webPath to urlStr ?
+    * urlStr:"https://www.google.com/dev/push?=someValue"
     */
-   public static func str(urlStr: String, onComplete:@escaping DownloadComplete = defaultDownloadComplete) {
+   public static func str(urlStr: String, httpMethod:HTTPMethodType = .get, onComplete:@escaping DownloadComplete = defaultDownloadComplete) {
       guard let url = URL(string: urlStr) else { onComplete(nil, .invalideWebPath); return }
       str(url: url, downloadComplete: onComplete)
    }
@@ -19,8 +21,8 @@ extension NetworkParser {
     * ## Examples:
     * NetworkParser.str(url: url)
     */
-   public static func str(url: URL, downloadComplete:@escaping DownloadComplete = defaultDownloadComplete) {
-      data(url: url) { data, response, error in
+   public static func str(url: URL, httpMethod:HTTPMethodType = .get, downloadComplete:@escaping DownloadComplete = defaultDownloadComplete) {
+      data(url: url, httpMethod: httpMethod) { data, response, error in
          guard let data = data, error == nil else { downloadComplete(nil, .errorGettingDataFromURL(error, response)); return }
          //Swift.print(response?.suggestedFilename ?? url.lastPathComponent)
          guard let stringValue = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as String? else { downloadComplete(nil, .dataIsNotString); return }
